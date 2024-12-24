4c0169d66d9e011d9cba6333d3a809d8796681ef
B-lanc <Jerry.Hatulusan.Tobing@Gmail.com>
2024-12-24 11:20:35 +0100

    Added Notch
    Tested and nulls with reaEQ

---
+++ tests/test_Notch.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+sys.path.insert(0, os.getcwd())
+from utils import read_audio, write_audio
+from filters import Notch
+
+PATH = "./audio/testaudio.wav"
+FREQ = 2000
+Q = 2
+WRITEPATH = f"./audio/testNotchf{FREQ}q{Q}.wav"
+
+
+def test_HPF():
+    data, sr = read_audio(PATH)
+    data = Notch(data, FREQ, sr, Q, 0)
+    write_audio(WRITEPATH, data, sr)
+
+
+test_HPF()
